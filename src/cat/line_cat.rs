990cc126e271d87235e8633f7e36e877d6c0818b
Scott <scott.geigel@gmail.com>
2016-12-23 21:48:07 -0600

    another formatting mistake in line number labels still getting some incorrect responses when running -Abs on larger input

--- src/cat/line_cat.rs
+++ src/cat/line_cat.rs
@@ -8,9 +8,7 @@ pub struct LineCat {
     //state variables
     in_line : bool,
     last_line_blank : bool,
-    line_buffer : Vec<u8>,
     line_number : usize,
-    char_pos : usize,
 }
 
 impl LineCat {
@@ -20,42 +18,39 @@ impl LineCat {
             squeeze_blanks : squeeze_blanks,
             in_line : false,
             last_line_blank : false,
-            line_buffer : Vec::new(),
             line_number : 1,
-            char_pos : 0,
         }
     }
 }
 
 impl CatMethod for LineCat {
     fn process_buffer(&mut self, input_buffer : &[u8], cat : &Box<CatFilter>) {
-        self.line_buffer.extend_from_slice(input_buffer);
-        while self.char_pos < self.line_buffer.len() {
+        let mut start : usize = 0;
+        let mut end : usize = 0;
+
+        while end < input_buffer.len() {
             if !self.in_line {
-                print!("     {}\t", self.line_number);
+                print!("{:6}\t", self.line_number);
                 self.in_line = true;
             }
-            if self.line_buffer[self.char_pos] == b'\n' {
-                self.char_pos += 1;
+            if input_buffer[end] == b'\n' {
+                end += 1;
 
-                let is_blank : bool = self.char_pos == 1;
+                let is_blank : bool = (end - start) == 1;
 
                 if (self.number_blanks || !is_blank) && !(self.squeeze_blanks && is_blank)  {
                     self.in_line = false;
                     self.line_number += 1;
-                    let (line, _) = self.line_buffer.split_at(self.char_pos);
-                    cat.filter_output(line);
+                    cat.filter_output(&input_buffer[start..end]);
                 }
 
-                self.line_buffer = self.line_buffer.split_off(self.char_pos);
-                self.char_pos = 0;
+                start = end;
                 self.last_line_blank = is_blank;
             } else {
-                self.char_pos += 1;
+                end += 1;
             }
         }
         //clear out the leftovers in the buffer
-        cat.filter_output(self.line_buffer.as_slice());
-        self.line_buffer.clear();
+        cat.filter_output(&input_buffer[start..]);
     }
 }
